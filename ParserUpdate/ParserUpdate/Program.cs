5a65f98306c3adb5733b639b57b1d204dcb1e8bc
Костя <kostapasenko76@gmail.com>
2022-12-13 17:57:41 +0200

    Try Catch конструкция и Цикл

--- ParserUpdate/ParserUpdate/Program.cs
+++ ParserUpdate/ParserUpdate/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Greetings();
+            string? Input;
+            ParserOutput output;
+            bool Complete = false;
+            while (true)
+            {
+                Input = Console.ReadLine();
+                try
+                {
+                    output = Parser.LetsParse(Input);
+                    break;
+                }
+                catch
+                {
+                Console.Clear();
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine("Ёще Раз Без Ошибок");
+                Greetings();
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine("Ваша команда");
+            Console.WriteLine($"Команда - <{output.Command}>; X - {output.X}; Y - {output.Y}");
+            Console.ReadKey();
+        }
+        static void Greetings()
+        {
+            Console.WriteLine("Введите Кординаты");
+            Console.WriteLine("Образец: <Открыть или Флаг> X Y");
+            Console.Write(":");
         }
     }
 }
